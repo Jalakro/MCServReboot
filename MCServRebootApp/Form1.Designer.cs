b7e0c5e8751e6ef1939ac12460890b1a19cf9959
Jalakro <a.poignet12@orange.fr>
2022-07-31 22:03:42 +0200

    add file reading/writing system to stock paths

--- MCServRebootApp/Form1.Designer.cs
+++ MCServRebootApp/Form1.Designer.cs
@@ -36,6 +36,14 @@
             this.myTimer = new System.Windows.Forms.Timer(this.components);
             this.button2 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -52,7 +60,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(145, 331);
+            this.button3.Location = new System.Drawing.Point(145, 519);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 27);
             this.button3.TabIndex = 5;
@@ -92,7 +100,7 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button4.Location = new System.Drawing.Point(125, 231);
+            this.button4.Location = new System.Drawing.Point(125, 218);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(115, 30);
             this.button4.TabIndex = 8;
@@ -100,13 +108,93 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(95, 332);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Save";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(187, 332);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Cancel";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(95, 426);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Save";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(187, 426);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Cancel";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 303);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 23);
+            this.textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(95, 397);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(167, 23);
+            this.textBox2.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(141, 285);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 15);
+            this.label1.TabIndex = 15;
+            this.label1.Text = ".bat File Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 379);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 15);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Backups Destination Directory";
+            // 
             // Form1
             // 
             this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.button3;
-            this.ClientSize = new System.Drawing.Size(360, 370);
+            this.ClientSize = new System.Drawing.Size(360, 558);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.checkBox1);
@@ -129,5 +217,13 @@
         private System.Windows.Forms.Timer myTimer;
         private Button button2;
         private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
     }
 }
--- MCServRebootApp/Form1.cs
+++ MCServRebootApp/Form1.cs
@@ -1,11 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
-using System.IO;
-using System.Windows.Forms;
 using System.IO.Compression;
 
 namespace MCServRebootApp
@@ -13,13 +6,16 @@ namespace MCServRebootApp
     public partial class Form1 : Form
     {
 
-        string path = "E:/Jeux/serv_14.01.22/start.bat";
+        string path;
         string process_name = "Java";
 
-        string backup_source_folder = "E:/Jeux/serv_14.01.22";
-        string backup_dest_path = "F:/Jeux/Servs_MC/Backups";
+        string backup_source_folder;
+        string backup_dest_path;
 
         bool isRunning = false;
+
+        string datapath;
+        string datafileName = "data.txt";
 
         public Form1()
         {
@@ -28,11 +24,49 @@ namespace MCServRebootApp
 
             if (!File.Exists("E:/Jeux/serv_14.01.22/RebootLogs.txt")) // If file does not exists
                 File.Create("E:/Jeux/serv_14.01.22/RebootLogs.txt").Close(); // Create file
+
+
+            datapath = Path.Combine(Environment.CurrentDirectory, datafileName);
+
+            if (!File.Exists(datapath)) // If file does not exists
+                File.Create(datapath).Close(); // Create file
+
+            if (new FileInfo(datapath).Length > 0)
+            {
+                System.IO.StreamReader fileReader;
+                fileReader = new System.IO.StreamReader(datapath);
+
+                //string vide = fileReader.ReadLine() + "\r\n";
+
+                path = fileReader.ReadLine();
+                backup_dest_path = fileReader.ReadLine() + "\r\n";
+                
+                textBox1.Text = path;
+                textBox2.Text = backup_dest_path;
+
+                if (path.Length > 0)
+                {
+                    int count = 0;
+                    char charToReset = '/';
+                    foreach (char c in path)
+                    {
+                        count++;
+                        if (c == charToReset)
+                        {
+                            count = 0;
+                        }
+                    }
+
+                    backup_source_folder = path.Remove(path.Length - count - 1, count + 1);
+                }
+
+                fileReader.Close();
+            }
         }
 
 
         private void InitializeTimer()
-        { 
+        {
             // Set to 1 second  
             myTimer.Interval = 1000;
             myTimer.Tick += new EventHandler(myTimer_Tick);
@@ -54,7 +88,7 @@ namespace MCServRebootApp
             if (isRunning == false)
             {
                 Process.Start(path);
-                using (StreamWriter sw = File.AppendText("E:/Jeux/serv_14.01.22/RebootLogs.txt"))
+                using (StreamWriter sw = File.AppendText(backup_source_folder + "/RebootLogs.txt"))
                 {
                     sw.WriteLine("Reboot successful on : " + DateTime.UtcNow.ToString("D") + "\t" + DateTime.Now.ToString("hh:mm:ss")); // Write text to .txt file
                 }
@@ -63,108 +97,129 @@ namespace MCServRebootApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (button1.Text == "Stop")
+            if (string.IsNullOrEmpty(path) || string.IsNullOrEmpty(backup_dest_path) || String.IsNullOrEmpty(textBox1.Text) || String.IsNullOrEmpty(textBox2.Text))
             {
-                button1.Text = "Start";
-                button1.BackColor = Color.Chartreuse;
-                myTimer.Enabled = false;
-                checkBox1.Checked = false;
+                MessageBox.Show("Please fill both the .bat path and the backup destination directory path before activating the bot");
             }
             else
             {
-                button1.Text = "Stop";
-                button1.BackColor = Color.OrangeRed;
-                myTimer.Enabled = true;
-                checkBox1.Checked = true;
+                if (button1.Text == "Stop")
+                {
+                    button1.Text = "Start";
+                    button1.BackColor = Color.Chartreuse;
+                    myTimer.Enabled = false;
+                    checkBox1.Checked = false;
+                }
+                else
+                {
+                    button1.Text = "Stop";
+                    button1.BackColor = Color.OrangeRed;
+                    myTimer.Enabled = true;
+                    checkBox1.Checked = true;
+                }
             }
         }
 
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (Process.GetProcessesByName(process_name).Length > 0)
+            if (string.IsNullOrEmpty(path) || string.IsNullOrEmpty(backup_dest_path) || String.IsNullOrEmpty(textBox1.Text) || String.IsNullOrEmpty(textBox2.Text))
             {
-                foreach (var process in Process.GetProcessesByName(process_name))
+                MessageBox.Show("Please fill both the .bat path and the backup destination directory path before activating the bot");
+            }
+            else
+            {
+                if (Process.GetProcessesByName(process_name).Length > 0)
                 {
-                    process.Kill();
+                    foreach (var process in Process.GetProcessesByName(process_name))
+                    {
+                        process.Kill();
+                    }
+
+                    Process.Start(path);
+
+                    using (StreamWriter sw = File.AppendText(backup_source_folder + "/RebootLogs.txt"))
+                    {
+                        sw.WriteLine("//FORCED// Reboot successful on : " + DateTime.UtcNow.ToString("D") + "\t" + DateTime.Now.ToString("hh:mm:ss")); // Write text to .txt file
+                    }
+
+                    MessageBox.Show("//FORCED// Reboot successful on : " + DateTime.UtcNow.ToString("D") + "\t" + DateTime.Now.ToString("hh:mm:ss"));
                 }
-
-                Process.Start(path);
-
-                using (StreamWriter sw = File.AppendText("E:/Jeux/serv_14.01.22/RebootLogs.txt"))
-                {
-                    sw.WriteLine("//FORCED// Reboot successful on : " + DateTime.UtcNow.ToString("D") + "\t" + DateTime.Now.ToString("hh:mm:ss")); // Write text to .txt file
-                }
-
-                MessageBox.Show("//FORCED// Reboot successful on : " + DateTime.UtcNow.ToString("D") + "\t" + DateTime.Now.ToString("hh:mm:ss"));
             }
         }
 
 
         private void button4_Click(object sender, EventArgs e)
         {
-            //stop du relaunch auto pour pas qu'il relance le serv pendant la backup
-            if (checkBox1.Checked == true)
+            if (string.IsNullOrEmpty(path) || string.IsNullOrEmpty(backup_dest_path) || String.IsNullOrEmpty(textBox1.Text) || String.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Please fill both the .bat path and the backup destination directory path before activating the bot");
+            }
+            else
+            {
+                //stop du relaunch auto pour pas qu'il relance le serv pendant la backup
+                if (checkBox1.Checked == true)
+                    button1_Click(sender, e);
+
+
+                //kill des instances du serv avant la backup
+                if (Process.GetProcessesByName(process_name).Length > 0)
+                {
+                    foreach (var process in Process.GetProcessesByName(process_name))
+                    {
+                        process.Kill();
+                    }
+                }
+
+
+                string filename = null;
+                string destfile = null;
+                string full_backup_dest_path = backup_dest_path + "/save_" + DateTime.Now.ToString("dd-MM-yyyy_hh-mm-ss");
+
+                //creation d'un nouveau dossier
+                Directory.CreateDirectory(full_backup_dest_path);
+
+                //copie du contenu du dossier du serv et collage dans le dossier de la backup
+                if (System.IO.Directory.Exists(backup_source_folder))
+                {
+                    string[] files = System.IO.Directory.GetFiles(backup_source_folder);
+
+                    // Copy the files and overwrite destination files if they already exist.
+                    foreach (string s in files)
+                    {
+                        // Use static Path methods to extract only the file name from the path.
+                        filename = System.IO.Path.GetFileName(s);
+                        destfile = System.IO.Path.Combine(full_backup_dest_path, filename);
+                        System.IO.File.Copy(s, destfile, true);
+                    }
+                }
+
+                //zip
+                ZipFile.CreateFromDirectory(full_backup_dest_path, full_backup_dest_path + ".zip");
+
+                //suppression du dossier pas zippé
+                if (Directory.Exists(full_backup_dest_path))
+                {
+                    string[] files = System.IO.Directory.GetFiles(full_backup_dest_path);
+                    foreach (string s in files)
+                    {
+                        File.Delete(s);
+                    }
+                    Directory.Delete(full_backup_dest_path);
+                }
+
+                //relance et reactive l'auto relaunch
+                Process.Start(path);
                 button1_Click(sender, e);
 
-
-            //kill des instances du serv avant la backup
-            if (Process.GetProcessesByName(process_name).Length > 0)
-            {
-                foreach (var process in Process.GetProcessesByName(process_name))
+                //msg l'user
+                using (StreamWriter sw = File.AppendText(backup_source_folder + "/RebootLogs.txt"))
                 {
-                    process.Kill();
+                    sw.WriteLine("SERVER BACKUP then Reboot successful on : " + DateTime.UtcNow.ToString("D") + "\t" + DateTime.Now.ToString("hh:mm:ss")); // Write text to .txt file
                 }
+
+                MessageBox.Show("SERVER BACKUP then Reboot successful on : " + DateTime.UtcNow.ToString("D") + "\t" + DateTime.Now.ToString("hh:mm:ss"));
             }
-
-            Thread.Sleep(5000);
-
-            string filename = null;
-            string destfile = null;
-            string full_backup_dest_path = backup_dest_path + "/save_" + DateTime.Now.ToString("dd-MM-yyyy_hh-mm-ss");
-
-            //creation d'un nouveau dossier
-            Directory.CreateDirectory(full_backup_dest_path);
-
-            //copie du contenu du dossier du serv et collage dans le dossier de la backup
-            if (System.IO.Directory.Exists(backup_source_folder))
-            {
-                string[] files = System.IO.Directory.GetFiles(backup_source_folder);
-
-                // Copy the files and overwrite destination files if they already exist.
-                foreach (string s in files)
-                {
-                    // Use static Path methods to extract only the file name from the path.
-                    filename = System.IO.Path.GetFileName(s);
-                    destfile = System.IO.Path.Combine(full_backup_dest_path, filename);
-                    System.IO.File.Copy(s, destfile, true);
-                }
-            }
-
-            //zip
-            ZipFile.CreateFromDirectory(full_backup_dest_path, full_backup_dest_path + ".zip");
-
-            //suppression du dossier pas zippé
-            if (Directory.Exists(full_backup_dest_path))
-            {
-                string[] files = System.IO.Directory.GetFiles(full_backup_dest_path);
-                foreach (string s in files)
-                {
-                    File.Delete(s);
-                }
-                Directory.Delete(full_backup_dest_path);
-            }
-
-            //relance et reactive l'auto relaunch
-            Process.Start(path);
-            button1_Click(sender, e);
-
-            //msg l'user
-            using (StreamWriter sw = File.AppendText("E:/Jeux/serv_14.01.22/RebootLogs.txt"))
-            {
-                sw.WriteLine("SERVER BACKUP then Reboot successful on : " + DateTime.UtcNow.ToString("D") + "\t" + DateTime.Now.ToString("hh:mm:ss")); // Write text to .txt file
-            }
-            MessageBox.Show("SERVER BACKUP then Reboot successful on : " + DateTime.UtcNow.ToString("D") + "\t" + DateTime.Now.ToString("hh:mm:ss"));
         }
 
 
@@ -175,6 +230,7 @@ namespace MCServRebootApp
         }
 
 
+        #region
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -182,6 +238,111 @@ namespace MCServRebootApp
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
 
+        }
+        #endregion
+
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (new FileInfo(datapath).Length > 0)
+            {
+                if (textBox1.Text.Length > 0)
+                {
+                    path = textBox1.Text;
+
+                    File.Create(datapath).Close();
+                    using (StreamWriter sw = File.AppendText(datapath))
+                    {
+                        sw.WriteLine(path);
+                        sw.WriteLine(backup_dest_path);
+                        sw.Close();
+                    }
+                }
+                else
+                {
+                    textBox1.Text = path;
+                }
+            }
+            else
+            {
+                if (textBox1.Text.Length > 0)
+                {
+                    path = textBox1.Text;
+
+                    File.Create(datapath).Close();
+                    using (StreamWriter sw = File.AppendText(datapath))
+                    {
+                        sw.WriteLine(path);
+                        sw.WriteLine(backup_dest_path);
+                        sw.Close();
+                    }
+                }
+            }
+
+            if (path.Length > 0)
+            {
+                int count = 0;
+                char charToReset = '/';
+                foreach (char c in path)
+                {
+                    count++;
+                    if (c == charToReset)
+                    {
+                        count = 0;
+                    }
+                }
+
+                backup_source_folder = path.Remove(path.Length - count - 1, count + 1);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = path;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (new FileInfo(datapath).Length > 0)
+            {
+                if (textBox2.Text.Length > 0)
+                {
+                    backup_dest_path = textBox2.Text;
+
+                    File.Create(datapath).Close();
+                    using (StreamWriter sw = File.AppendText(datapath))
+                    {
+                        sw.WriteLine(path);
+                        sw.WriteLine(backup_dest_path);
+                        sw.Close();
+                    }
+                }
+                else
+                {
+                    textBox2.Text = backup_dest_path;
+                }
+            }
+            else
+            {
+                if (textBox2.Text.Length > 0)
+                {
+                    backup_dest_path = textBox2.Text;
+
+                    File.Create(datapath).Close();
+                    using (StreamWriter sw = File.AppendText(datapath))
+                    {
+                        sw.WriteLine(path);
+                        sw.WriteLine(backup_dest_path);
+                        sw.Close();
+                    }
+                }
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = backup_dest_path;
         }
     }
 }
